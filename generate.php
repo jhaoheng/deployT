218da5042ffcdf9368d158404ed213aad7cc8b38
maxhu <jhaoheng@gmail.com>
2016-04-07 19:25:35 +0800

    update

--- generate.php
+++ generate.php
@@ -1,5 +1,8 @@
 <?php
 /*
+ex:
+generate.php?folder_name=
+
 產生 token
 1. 輸入 foder name
 2. folder_name+日期 進行 base64_encode -> token
@@ -25,12 +28,19 @@ echo "【Webhook is】 ";
 echo '<br><br>';
 echo $_SERVER['HTTP_HOST'].$path_parts['dirname']."/deploy.php?token=".$str."<br>";
 
-// since PHP 5.2.0
-// pathinfo($_SERVER['REQUEST_URI']);
-// var_dump($_SERVER);
+echo "<br>";
+echo "1. 透過 generate.php 取得 token<br>";
+echo "2. 將 token 設定於 config.php<br>";
+echo "3. 初始化 deploy repo<br>";
+echo "4. 將 (1) 中取得的 webhook 設定於 gitlab or github<br>";
+echo "5. 於網址中，測試 (1) 中得到 webhook<br>";
 
-/*
-是否在此建立 repo 的資料夾與 remote 的位置？
-*/
+echo "<br>";
+echo "<hr>";
+echo "注意若測試失敗<br>"
+echo "1. 請確認用的是 ssh 還是 https<br>";
+echo "2. 確認 config 中的路徑設定<br>";
+
+
 
 ?>
